8163986c5e1fe154853e420736ac1325fddc6563
Chathura Buddhika <chathu.buddi@gmail.com>
2018-07-20 17:42:41 +0530

    finishing up app

--- app/src/main/java/lk/chathurabuddi/interactivestory/ui/MainActivity.java
+++ app/src/main/java/lk/chathurabuddi/interactivestory/ui/MainActivity.java
@@ -32,6 +32,12 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        nameEditText.setText("");
+    }
+
     private void startStory(String name) {
         Intent intent = new Intent(this, StoryActivity.class);
         Resources resources = getResources(); // this is optional
